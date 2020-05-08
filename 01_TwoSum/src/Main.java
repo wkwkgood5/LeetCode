9d4360eba04915f318c3fdbd55ffcf473a5cb214
Kang Wang <p95.kangwang@gmail.com>
2020-05-09 00:24:55 +0800

    no message

--- 01_TwoSum/src/Main.java
+++ 01_TwoSum/src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("hello");
+	    int[] nums = new int[]{2, 7, 11, 15};
+	    int target = 18;
+        int[] result = new int[2];
+        Solution_force solution=new Solution_force();
+        result = solution.twoSum(nums, target);
+	    System.out.println("[" + result[0]+"," + result[1] + "]");
     }
 }
--- 01_TwoSum/src/Solution_force.java
+++ 01_TwoSum/src/Solution_force.java
@@ -1,4 +1,6 @@
-public class Solution {
+//暴力法，执行用时:88 ms, 在所有 Java 提交中击败了17.90%的用户。内存消耗:40.3 MB, 在所有 Java 提交中击败了5.06%的用户
+
+public class Solution_force {
     public int[] twoSum(int[] nums, int target) {
         int[] index = new int[2];
         for (int i=0; i<nums.length; i++){
