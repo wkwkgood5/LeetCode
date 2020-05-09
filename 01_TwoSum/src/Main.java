ef410492fa82d6c9ed7570726134231f889a58db
Kang Wang <p95.kangwang@gmail.com>
2020-05-09 22:49:07 +0800

    no message

--- 01_TwoSum/src/Main.java
+++ 01_TwoSum/src/Main.java
@@ -2,10 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
 	    int[] nums = new int[]{2, 7, 11, 15};
-	    int target = 18;
-        int[] result = new int[2];
-        Solution_force solution=new Solution_force();
-        result = solution.twoSum(nums, target);
-	    System.out.println("[" + result[0]+"," + result[1] + "]");
+
+	    int target_force = 18;
+        int[] result_force = new int[2];
+        Solution_force solution_force=new Solution_force();
+        result_force = solution_force.twoSum(nums, target_force);
+	    System.out.println("Solution_force: [" + result_force[0]+"," + result_force[1] + "]");
+
+        int target_hashmap = 17;
+        int[] result_hashmap = new int[2];
+        Solution_hashmap solution_hashmap=new Solution_hashmap();
+        result_hashmap = solution_hashmap.twoSum(nums, target_hashmap);
+        System.out.println("Solution_hashmap: [" + result_hashmap[0]+"," + result_hashmap[1] + "]");
     }
 }
--- 01_TwoSum/src/Solution_hashmap.java
+++ 01_TwoSum/src/Solution_hashmap.java
@@ -1,2 +1,27 @@
+//思想：利用hashmap，只遍历一遍，根据遍历值，去hashmap里寻找与之匹配的值，若有，返回各自的key，若没有，将此遍历值放入hashmap中
+
+import java.util.HashMap;
+
 public class Solution_hashmap {
+    public int[] twoSum(int[] nums, int target){
+        int[] res = new int[2];
+        if (nums == null || nums.length<=1){
+            return res;
+        }
+
+        HashMap<Integer, Integer> map= new HashMap<Integer, Integer>();//创建hashmap，内部为空元素
+        for (int i=0; i<nums.length; i++){
+            int num = nums[i];
+            int value = target-num;
+            if (map.containsKey(value)){
+                res[1]=i;
+                res[0]=map.get(value);
+            }
+            else {
+                map.put(num, i);
+            }
+
+        }
+        return res;
+    }
 }
